0edf62d266ab4eb0833adaba5d76761c3de5db83
Daniel Kiss <kissd621@gmail.com>
2020-07-03 12:41:33 +0200

    Token length can be specified

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,9 +1,9 @@
 const crypto = require('crypto');
 
 // Generates random token and returns it along the hashed version
-module.exports = () => {
+module.exports = (length = 48) => {
   // Generate random token
-  const token = crypto.randomBytes(48).toString('hex');
+  const token = crypto.randomBytes(length).toString('hex');
 
   // Hash token using crypto
   const hashedToken = crypto.createHash('sha256').update(token).digest('hex');
